e94bebb2255f573811f24a3be2b6385a75bbc2f4
FerNikoMF <76095334+FerNikoMF@users.noreply.github.com>
2024-03-05 16:15:32 +0500

    Add files via upload

---
+++ Green Steam.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-multi-spaces */
+const btnList = [
+    { url: "https://steamrip.com/?s=", title: "SteamRIP" },
+    //{ url: "https://store.epicgames.com/en-US/browse?q=", title: "Epic games" },
+    { url: "https://rutracker.net/forum/tracker.php?nm=", title: "Rutracker" },
+];
+
+const siteUrls = [
+    { url: "https://store.steampowered.com/app/*", title: "Steam" },
+];
+
+// ==UserScript==
+// @name         Green Steam
+// @namespace    FerNikoMF & Muxammadaziz04
+// @author       FerNikoMF & Muxammadaziz04
+// @version      1.0.0
+// @description  Just adding a pirated link to all the games in the Steam store
+// @match        https://store.steampowered.com/app/*
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+let siteResult = "";
+
+siteUrls.forEach((el) => {
+    if (document.URL.match(el.url)) siteResult = el.title;
+});
+
+let appName = "";
+
+if(siteResult == 'Steam'){
+    appName = document.querySelector(".apphub_AppName").textContent.trim();
+    document.querySelector(".game_purchase_action_bg:first-child").style.cssText = "height: 50px; max-width: 500px; text-wrap: wrap";
+
+    btnList.forEach((el) => {
+        document.querySelector(".apphub_OtherSiteInfo").insertAdjacentElement("afterend", furnishSteamLink(el.url + appName, el.title));
+    });
+}
+
+function furnishSteamLink(href, text) {
+    const element = document.createElement("a");
+    element.target = "_blank";
+    element.classList.add("apphub_OtherSiteInfo");
+    element.style.cssText = "padding: 1px 15px; line-height: 30px; font-size: 15px; color: #67c1f5; background-color: rgba(103, 193, 245, 0.2); margin-right: 10px; border-radius: 2px;";
+    element.href = href;
+    element.innerText = text;
+    return element;
+}
