7b28d0ca58e97c1116d95faf509f2b4247e30514
Firdavs <76095334+FerNikoMF@users.noreply.github.com>
2024-05-21 23:45:06 +0500

    Improve script structure, readability, and reliability , added the ability to convert Roman numbers to Arabic numbers for the correct formation of links.
    - Replaced traditional function expressions with arrow functions for better readability and conciseness.
    - Added checks to ensure DOM elements exist before interacting with them to prevent potential errors.
    - Modularized the code by dividing it into well-named functions for better organization and maintainability.
    - Used 'const' and 'let' instead of 'var' to adhere to modern JavaScript best practices.
    - Improved CSS style string readability by using template literals.
    - Encapsulated the code within an Immediately Invoked Function Expression (IIFE) to minimize global scope pollution.
    - Updated textToSlug function to remove special characters such as apostrophes and colons.
    - Added isRomanNumeral function to detect Roman numerals.
    - Added the ability to convert Roman numbers to Arabic numbers for the correct formation of links.
    - This commit implements a feature to convert Roman numerals to Arabic numbers in game titles. This ensures that the generated links are correctly formatted, improving the functionality of the script.

--- dist/Green-Steam.js
+++ dist/Green-Steam.js
@@ -1,61 +1,106 @@
 // ==UserScript==
 // @name         Green Steam
 // @namespace    http://tampermonkey.net/
+// @version      1.1.2
 // @author       FerNikoMF & Muxammadaziz04
-// @version      1.1.1
 // @description  Just adding a pirated link to all the games in the Steam store
 // @match        https://store.steampowered.com/app/*
 // @homepage     https://github.com/FerNikoMF/Green-Steam
-// @homepageURL  https://github.com/FerNikoMF/Green-Steam
-// @downloadURL  hhttps://raw.githubusercontent.com/FerNikoMF/Green-Steam/main/dist/Green-Steam.js
+// @downloadURL  https://raw.githubusercontent.com/FerNikoMF/Green-Steam/main/dist/Green-Steam.js
 // @updateURL    https://raw.githubusercontent.com/FerNikoMF/Green-Steam/main/dist/Green-Steam.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @license      MIT
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
+// @icon         https://raw.githubusercontent.com/FerNikoMF/Green-Steam/main/green.png
 // @grant        none
-// @license      MIT
 // ==/UserScript==
 
 (function() {
+    'use strict';
+
     const btnList = [
-    { url: "https://www.playground.ru/", title: "Playground", withSlug: true },
-    { url: "https://rutracker.net/forum/tracker.php?nm=", title: "Rutracker" },
-];
+        { url: "https://www.playground.ru/", title: "Playground", withSlug: true },
+        { url: "https://rutracker.net/forum/tracker.php?nm=", title: "Rutracker" }
+    ];
 
-const siteUrls = [
-    { url: "https://store.steampowered.com/app/*", title: "Steam" },
-];
+    const siteUrls = [
+        { url: "https://store.steampowered.com/app/*", title: "Steam" }
+    ];
 
-    function textToSlug(text) {
-    if(typeof text !== 'string') return ''
-    return text?.toLowerCase()?.split(' ')?.join('_')
-}
+    const textToSlug = (text) => {
+        if (typeof text !== 'string') return '';
+        return text
+            .toLowerCase()
+            .replace(/[\u2019':]/g, '') // Remove special characters like ’ and :
+            .replace(/\s+/g, '_'); // Replace spaces with underscores
+    };
 
-function furnishSteamLink(href, text) {
-    const element = document.createElement("a");
-    element.target = "_blank";
-    element.classList.add("apphub_OtherSiteInfo");
-    element.style.cssText = "padding: 1px 15px; line-height: 30px; font-size: 15px; color: #67c1f5; background-color: rgba(103, 193, 245, 0.2); margin-right: 10px; border-radius: 2px;";
-    element.href = href;
-    element.innerText = text;
-    return element;
-}
+    const romanToArabic = (roman) => {
+        const romanNumerals = {
+            I: 1,
+            V: 5,
+            X: 10,
+            L: 50,
+            C: 100,
+            D: 500,
+            M: 1000
+        };
 
-let siteResult = "";
+        let result = 0;
+        let prevValue = 0;
 
-siteUrls.forEach((el) => {
-    if (document.URL.match(el.url)) siteResult = el.title;
-});
+        for (let i = roman.length - 1; i >= 0; i--) {
+            const value = romanNumerals[roman[i]];
+            if (value < prevValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+            prevValue = value;
+        }
 
-let appName = "";
+        return result;
+    };
 
-if(siteResult == 'Steam'){
-    appName = document.querySelector(".apphub_AppName").textContent.trim();
-    document.querySelector(".game_purchase_action_bg:first-child").style.cssText = "height: 32px; max-width: 500px; text-wrap: wrap";
+    const furnishSteamLink = (href, text) => {
+        const element = document.createElement('a');
+        element.target = '_blank';
+        element.classList.add('apphub_OtherSiteInfo');
+        element.style.cssText = 'padding: 1px 15px; line-height: 30px; font-size: 15px; color: #67c1f5; background-color: rgba(103, 193, 245, 0.2); margin-right: 10px; border-radius: 2px;';
+        element.href = href;
+        element.innerText = text;
+        return element;
+    };
 
-    btnList.forEach((el) => {
-        const url = el?.withSlug ? el.url + textToSlug(appName) : el.url + appName
-        document.querySelector(".apphub_OtherSiteInfo").insertAdjacentElement("afterend", furnishSteamLink(url, el.title));
-    });
-}
+    const isMatchingSite = (url) => {
+        return siteUrls.some(site => document.URL.match(site.url));
+    };
+
+    const getAppName = () => {
+        const appNameElement = document.querySelector('.apphub_AppName');
+        return appNameElement ? appNameElement.textContent.trim() : '';
+    };
+
+    const insertButtons = (appName) => {
+        const container = document.querySelector('.game_purchase_action_bg:first-child');
+        if (container) {
+            container.style.cssText = 'height: 32px; max-width: 500px; text-wrap: wrap';
+
+            btnList.forEach(btn => {
+                const appNameWithoutRomanNumerals = appName.replace(/\b([IVXLCDM]+)\b/g, (match, roman) => romanToArabic(roman));
+                const url = btn.withSlug ? `${btn.url}${textToSlug(appNameWithoutRomanNumerals)}` : `${btn.url}${appNameWithoutRomanNumerals}`;
+                const linkElement = furnishSteamLink(url, btn.title);
+                const targetElement = document.querySelector('.apphub_OtherSiteInfo');
+                if (targetElement) {
+                    targetElement.insertAdjacentElement('afterend', linkElement);
+                }
+            });
+        }
+    };
+
+    if (isMatchingSite(document.URL)) {
+        const appName = getAppName();
+        if (appName) {
+            insertButtons(appName);
+        }
+    }
 })();
